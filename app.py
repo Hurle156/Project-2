18f8198c8c5000fec5070f937b735743de1befa8
Spencer Hurley <hurle156@umn.edu>
2020-04-09 21:17:01 -0500

    super basic template for HTML

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 engine = create_engine("sqlite:///Dataset/drafts.sqlite")
 
@@ -19,18 +19,30 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """Someone opened this site, wow."""
+    webpage = render_template("index.html")
 
+    return webpage
+
+@app.route("/all-data")
+def data():
     session = Session(engine)
 
     results = session.query(draft_data)
-
     session.close()
     Test = []
     for r in results:
         pick = {}
+        pick["year"] = r.Year
         pick["id"] = r.Id
-        pick["Pick"] = r.Pick
+        pick["round"] = r.Rd
+        pick["pick"] = r.Pick
+        pick["team"] = r.Team
         pick["name"] = r.Player
+        pick["pos"] = r.Pos
+        pick["hs"] = r.HS
+        pick["st"] = r.St
+        pick["signed"] = r.Signed
+        pick["bonus"] = r.Bonus
         Test.append(pick)
     return jsonify(Test)
 
